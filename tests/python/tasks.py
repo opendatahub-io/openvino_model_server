212007dfcf32b713fd9380373ea40afe969f7d42
Syed Nomaan Ali <syedali@redhat.com>
2026-07-07 16:59:28 +0530

    Sync 2026.2 content to stable (preserving .tekton and .github/workflows)
    Signed-off-by: Syed Nomaan Ali <syedali@redhat.com>
    
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED

--- tests/python/tasks.py
+++ tests/python/tasks.py
@@ -15,8 +15,8 @@
 #
 from invoke import task
 
-supported_clients = {"kserve-api":["3.9","3.10","3.11","3.12","3.13"],
-                     "tensorflow-serving-api":["3.9","3.10","3.11","3.12"]}
+supported_clients = {"kserve-api":["3.10","3.11","3.12","3.13"],
+                     "tensorflow-serving-api":["3.10","3.11","3.12"]}
 
 @task
 def deps(c, verbose=False, fastFail=False):
